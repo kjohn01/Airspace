a31af26461cc06e7be2305366cca2841b309890b
mayacafe123 <mayacafe123@gmail.com>
2020-03-01 22:51:24 +0800

    basic set-up, isolating reducer, trim the code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import './App.css';
 import DragAndDrop from './DragAndDrop'
+import reducer from './reducer';
 
-function App() {
+const App = () => {
 
-  const reducer = (state, action) => {
-    switch (action.type) {
-      case 'SET_DROP_DEPTH':
-        return { ...state, dropDepth: action.dropDepth }
-      case 'SET_IN_DROP_ZONE':
-        return { ...state, inDropZone: action.inDropZone };
-      case 'ADD_FILE_TO_LIST':
-        return { ...state, fileList: state.fileList.concat(action.files) };
-      default:
-        return state;
-    }
-  };
-
-  const [data, dispatch] = React.useReducer(
+  const [data, dispatch] = useReducer(
     reducer, { dropDepth: 0, inDropZone: false, fileList: [] }
   )
 
@@ -25,7 +13,6 @@ function App() {
     <div className="App">
       <h1>React drag and drop component</h1>
       <DragAndDrop data={data} dispatch={dispatch} />
-
       <ol className="dropped-files">
         {data.fileList.map(f => {
           return (
