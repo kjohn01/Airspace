d934342c43e28b12c1418ee46e781309837da8df
Ting-Hsien Wang <ting-hsienwang@pc-6-239.customer.ask4.lan>
2020-04-02 00:05:13 +0100

    testing config

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,2 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
-
-test('renders page heading', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/React drag and drop component/i);
-  expect(linkElement).toBeInTheDocument();
-});
