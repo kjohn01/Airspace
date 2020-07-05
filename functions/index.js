37ae348f133919736885bdee8b59bed7bcdea58a
Ting-Hsien Wang <mayacafe123@gmail.com>
2020-07-05 22:41:58 +0100

    update file info on firestore when a file is uploaded

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,22 @@
 const functions = require('firebase-functions');
+const path = require('path');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// The Firebase Admin SDK to access Cloud Firestore.
+const admin = require('firebase-admin');
+
+admin.initializeApp();
+const db = admin.firestore();
+
+exports.updateFile = functions.storage.object().onFinalize(async (object) => {
+  const filePath = object.name; 
+  const name = path.basename(filePath);
+  const uid = path.dirname(filePath);
+  const data = {
+    name,
+    type: object.contentType, 
+    createdAt: object.timeCreated,
+    lastModified: object.updated,
+    size: object.size,
+  };
+  return await db.collection('users').doc(uid).collection('files').doc(name).set(data);
+});
