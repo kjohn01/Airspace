a6fafc2d9293291bb8eb71ad99e42dba1eb249a8
Ting-Hsien Wang <mayacafe123@gmail.com>
2020-11-17 14:19:03 +0000

    enable pwa

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable import/no-named-as-default */
+/* eslint-disable import/no-named-as-default-member */
 /* eslint-disable max-len */
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -14,4 +16,4 @@ ReactDOM.unstable_createRoot(document.getElementById('root')).render(<GlobalAuth
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
